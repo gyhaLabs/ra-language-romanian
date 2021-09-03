5f529ebe8c777468c704d83bc92b40f923e5e178
Paul-Sebastian Manole <brokenthorn@gmail.com>
2021-09-03 15:48:11 +0300

    fix: update for latest react-admin version

--- src/index.ts
+++ src/index.ts
@@ -1,41 +1,38 @@
-import { TranslationMessages } from "ra-core";
+import {TranslationMessages} from "ra-core";
 
 const romanianMessages: TranslationMessages = {
     ra: {
         action: {
-            add_filter: "Adaugă filtru",
             add: "Adăugă",
+            add_filter: "Adaugă filtru",
             back: "Înapoi",
-            bulk_actions:
-                "1 articol selectat |||| %{smart_count} articole selectate",
+            bulk_actions: "1 articol selectat |||| %{smart_count} articole selectate",
             cancel: "Anulează",
             clear_input_value: "Șterge valoarea",
             clone: "Copiază",
+            close: "Închide",
+            close_menu: "Închide meniu",
             confirm: "Confirmă",
             create: "Creează",
+            create_item: "Creează element",
             delete: "Șterge",
             edit: "Editează",
+            expand: "Extinde",
             export: "Exportă",
             list: "Listează",
+            open_menu: "Deschide meniu",
             refresh: "Reîncarcă",
-            remove_filter: "Șterge acest filtru",
             remove: "Șterge",
+            remove_filter: "Șterge acest filtru",
             save: "Salvează",
             search: "Caută",
             show: "Arată",
             sort: "Sortează",
             undo: "Anulează",
             unselect: "Deselectează",
-            expand: "Extinde",
-            close: "Închide",
-            open_menu: "Deschide meniu",
-            close_menu: "Închide meniu",
+            update: "Actualizează",
         },
-        boolean: {
-            true: "Da",
-            false: "Nu",
-            null: "",
-        },
+        boolean: {true: "Da", false: "Nu", null: "",},
         page: {
             create: "Creează %{name}",
             dashboard: "Dashboard",
@@ -50,55 +47,42 @@ const romanianMessages: TranslationMessages = {
         },
         input: {
             file: {
-                upload_several:
-                    "Trageți aici fișiere pentru a le încărca sau clic pentru a selecta unul.",
-                upload_single:
-                    "Trageți aici un fișier pentru a-l încărca sau clic pentru a selecta unul.",
+                upload_several: "Trageți aici fișiere pentru a le încărca sau clic pentru a selecta unul.",
+                upload_single: "Trageți aici un fișier pentru a-l încărca sau clic pentru a selecta unul.",
             },
             image: {
-                upload_several:
-                    "Trageți aici imagini pentru a le încărca sau clic pentru a selecta una.",
-                upload_single:
-                    "Trageți aici o imagine pentru a o încărca sau clic pentru a selecta una.",
+                upload_several: "Trageți aici imagini pentru a le încărca sau clic pentru a selecta una.",
+                upload_single: "Trageți aici o imagine pentru a o încărca sau clic pentru a selecta una.",
             },
             references: {
                 all_missing: "Nu se pot găsi datele referențiate.",
-                many_missing:
-                    "Cel puțin una dintre referințele asociate nu mai este disponibilă.",
+                many_missing: "Cel puțin una dintre referințele asociate nu mai este disponibilă.",
                 single_missing: "Referința asociată nu mai este disponibilă.",
             },
-            password: {
-                toggle_visible: "Ascunde parola",
-                toggle_hidden: "Arată parola",
-            },
+            password: {toggle_visible: "Ascunde parola", toggle_hidden: "Arată parola",},
         },
         message: {
             about: "Despre",
             are_you_sure: "Sunteți sigur?",
-            bulk_delete_content:
-                "Sigur doriți să ștergeți articolul %{name}? |||| Sigur doriți să ștergeți aceste %{smart_count} articole?",
-            bulk_delete_title:
-                "Șterge %{name} |||| Șterge %{smart_count} %{name}",
+            bulk_delete_content: "Sigur doriți să ștergeți articolul %{name}? |||| Sigur doriți să ștergeți aceste %{smart_count} articole?",
+            bulk_delete_title: "Șterge %{name} |||| Șterge %{smart_count} %{name}",
             delete_content: "Sigur doriți să ștergeți acest articol?",
             delete_title: "Șterge %{name} #%{id}",
             details: "Detalii",
-            error:
-                "A apărut o eroare și solicitarea dvs. nu a putut fi finalizată",
+            error: "A apărut o eroare și solicitarea dvs. nu a putut fi finalizată",
             invalid_form: "Formularul nu este valid. Verificați erorile.",
             loading: "Pagina se încarcă, doar un moment...",
             no: "Nu",
-            not_found:
-                "Fie ați introdus o adresă URL greșită, fie ați urmărit un link necorespunzător",
+            not_found: "Fie ați introdus o adresă URL greșită, fie ați urmărit un link necorespunzător",
             yes: "Da",
-            unsaved_changes:
-                "Unele dintre modificările dvs. nu au fost salvate. Sunteți sigur că doriți să le ignorați?",
+            unsaved_changes: "Unele dintre modificările dvs. nu au fost salvate. Sunteți sigur că doriți să le ignorați?",
+            bulk_update_content: "Actualizează în masă conținutul",
+            bulk_update_title: "Actualizează în masă titlurile"
         },
         navigation: {
             no_results: "Nu au fost găsite rezultate",
-            no_more_results:
-                "Numărul de pagină %{page} este în afara limitelor. Încercați pagina anterioară.",
-            page_out_of_boundaries:
-                "Numărul de pagină %{page} este în afara limitelor",
+            no_more_results: "Numărul de pagină %{page} este în afara limitelor. Încercați pagina anterioară.",
+            page_out_of_boundaries: "Numărul de pagină %{page} este în afara limitelor",
             page_out_from_end: "Nu se poate merge după ultima pagină",
             page_out_from_begin: "Nu se poate merge înainte de pagina 1",
             page_range_info: "%{offsetBegin}-%{offsetEnd} din %{total}",
@@ -107,36 +91,28 @@ const romanianMessages: TranslationMessages = {
             prev: "Precedentul",
             skip_nav: "Sari la conținut"
         },
-        sort: {
-            sort_by: "Sortează după %{field} %{order}",
-            ASC: "ascendentă",
-            DESC: "descendentă"
-        },
+        sort: {sort_by: "Sortează după %{field} %{order}", ASC: "ascendentă", DESC: "descendentă"},
         auth: {
             auth_check_error: "Vă rugăm să vă autentificați pentru a continua",
             user_menu: "Profil",
             username: "Nume utilizator",
             password: "Parolă",
             sign_in: "Conectare",
-            sign_in_error:
-                "Autentificarea a eșuat. Vă rugăm să încercați din nou.",
+            sign_in_error: "Autentificarea a eșuat. Vă rugăm să încercați din nou.",
             logout: "Deconectare",
         },
         notification: {
-            updated:
-                "Articolul s-a actualizat |||| S-au actualizat %{smart_count} articole",
+            updated: "Articolul s-a actualizat |||| S-au actualizat %{smart_count} articole",
             created: "Articolul s-a creat",
-            deleted:
-                "Articolul s-a șters |||| S-au șters %{smart_count} articole",
+            deleted: "Articolul s-a șters |||| S-au șters %{smart_count} articole",
             bad_item: "Articol incorect",
             item_doesnt_exist: "Articolul nu există",
             http_error: "Eroare de comunicare cu serverul",
-            data_provider_error:
-                "Eroare dataProvider. Verificați detaliile în consolă.",
-            i18n_error:
-                "Traducerile nu pot fi încărcate pentru limba specificată",
+            data_provider_error: "Eroare dataProvider. Verificați detaliile în consolă.",
+            i18n_error: "Traducerile nu pot fi încărcate pentru limba specificată",
             canceled: "Acțiunea a fost anulată",
             logged_out: "Sesiunea dvs. s-a încheiat, vă rugăm să vă reconectați.",
+            not_authorized: "Nu sunteți autorizat"
         },
         validation: {
             required: "Obligatoriu",
@@ -147,8 +123,7 @@ const romanianMessages: TranslationMessages = {
             number: "Trebuie să fie un număr",
             email: "Trebuie să fie un e-mail valid",
             oneOf: "Trebuie să fie una dintre: %{options}",
-            regex:
-                "Trebuie să corespundă unui format specific (regex): %{pattern}",
+            regex: "Trebuie să corespundă unui format specific (regex): %{pattern}",
         },
     },
 };
